f2f7c81c2254a90e80d650f6965afb79b5aeb8c3
Kelly MacNeill <kelly@pyromuffin.com>
2020-12-05 02:38:58 -0800

    support nested arrays probably.

--- pdb_fixer.cpp
+++ pdb_fixer.cpp
@@ -18,22 +18,26 @@ int main(int argc, char** args)
     rewind(file);
 
     auto buffer = (char*)malloc(sizeof(char) * lSize);
+    if (!buffer)
+        return -1;
 
     // copy the file into the buffer:
     auto result = fread(buffer, 1, lSize, file);
     
     std::string replacement;
-    replacement.push_back('a');
-    replacement.push_back('<');
+
 
     int count = 0;
-    for (int i = 0; i < lSize - 2; i++)
+    for (int i = 0; i < lSize - 5; i++)
     {
         if (buffer[i] == '[' && buffer[i + 1] == ']' && buffer[i + 2] == ' ')
         {
             auto startIndex = i;
             auto endIndex = i + 3;
-            replacement.resize(2);
+            replacement.clear();
+
+            replacement.push_back('a');
+            replacement.push_back('<');
             while (buffer[endIndex] != '\0')
             {
                 replacement.push_back(buffer[endIndex]);
@@ -41,13 +45,41 @@ int main(int argc, char** args)
             }
             replacement.push_back('>');
 
-            fseek(file, i, SEEK_SET);
-            fputs(replacement.c_str(), file);
-            i = endIndex;
+            memcpy(&buffer[i], replacement.c_str(), replacement.size());
+
             count++;
         }
+
+        else if (buffer[i] == '[' && buffer[i + 1] == '.' && buffer[i + 2] == '.' && buffer[i + 3] == ']' && buffer[i + 4] == ' ')
+        {
+            auto startIndex = i;
+            auto endIndex = i + 5;
+            replacement.clear();
+            replacement.push_back('v');
+            replacement.push_back('e');
+            replacement.push_back('c');
+            replacement.push_back('<');
+            while (buffer[endIndex] != '\0')
+            {
+                replacement.push_back(buffer[endIndex]);
+                endIndex++;
+            }
+            replacement.push_back('>');
+
+            memcpy(&buffer[i], replacement.c_str(), replacement.size());
+
+            count++;
+        }
+
     }
 
+    if (count == 0)
+        goto done;
+
+    rewind(file);
+    fwrite(buffer, 1, lSize, file);
+
+    done:
     fclose(file);
     free(buffer);
 
